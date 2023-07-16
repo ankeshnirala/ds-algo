146ebd7a64e458201e074de63c633ea912fb818f
Ankesh <ankesh@Ankeshs-MacBook-Air.local>
2023-07-16 06:10:50 +0530

    string wip3

--- string/main.go
+++ string/main.go
@@ -31,5 +31,20 @@ func main() {
 
 	fmt.Println("Split - ", strings.Split(str1, ","))
 
-	fmt.Println("Replace - ", strings.Replace(str1, "is", "was"))
+	fmt.Println("Replace - ", strings.Replace(str1, "is", "was", 1))
+
+	newStr := "ankesh"
+	fmt.Println(len(newStr), newStr[0])
+	fmt.Println(65 + 26)
+
+	fmt.Println(strings.Index("Ankesh is", "s"))
+}
+
+func IndexOf(c string) int {
+	// Accepts character as argument, Returns index of
+	// the first occurrence of specified character
+
+	// str = "ankesh"
+
+	return 0
 }
